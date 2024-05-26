5017b8dd82e2809cfa1d9e57648ad2b18028d2ad
Hitendra Kumar Dewangan <148749063+HitenDewangan@users.noreply.github.com>
2024-05-26 17:13:13 +0530

    Delete Recursion directory

--- Recursion/Calc_length_string.py
+++
@@ -1,9 +0,0 @@
-def calc_len(str):
-    if str == "":
-        return 0
-    
-    length = 0
-    return length+1 + calc_len(str[1:])
-
-s = str(input("Enter a string: "))
-print("Length of string is: ",calc_len(s))
--- Recursion/Readme.txt
+++
@@ -1,1 +0,0 @@
-this is recursion practice questions for cp
