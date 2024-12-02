789315fe8af2e6586abe28e05c33e4fd63b72389
Hiten <hitendewangan100j@gmail.com>
2024-12-03 00:19:44 +0530

    upsolving problems

---
+++ CodeForces/Div2 Contest/2042B_GamewithColouredMarbles.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int>& marbles) {
+    unordered_map<int, int> mp;
+    
+    for (int i = 0; i < n; i++) {
+        mp[marbles[i]]++;
+    }
+    
+    if (mp.size() == 1) {  // if all are same
+        return 1;
+    }
+    
+    int unique = 0, ans = 0;
+    for(auto it : mp) {
+        if (it.second == 1) {
+            unique++;
+        }
+        else{
+            ans++;
+        }
+    }
+
+    ans += ((unique+1)/2) * 2;
+
+    
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin>>n;
+        
+        vector<int> marbles(n);
+        for (int i = 0; i < n; i++) {
+            cin >> marbles[i];
+        }
+        
+        cout << solve(n, marbles) << endl;
+    }
+    
+    return 0;
+}
